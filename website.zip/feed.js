30733ff5486fc8074faa6c94be416175589451e3
Varsha Madavaneri <163567950+Varshamadavaneri@users.noreply.github.com>
2025-06-18 12:04:12 +0530

    Delete website.zip directory

--- website.zip/feed.js
+++
@@ -1,6 +0,0 @@
-function submitFeedback(event) {
-    event.preventDefault();
-    document.getElementById("fb").style.display = "block";
-    document.getElementById("fb").innerHTML = "Thank you for your feedback.... :)))";
-    document.getElementById("feedback-form").reset();
-}
